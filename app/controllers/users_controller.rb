74dadc196d5fc836e9402a018c295f8e73694b99
Sensanaty <salevic@lu-ka.me>
2021-03-12 15:19:49 +0100

    Create the user :create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,17 @@
 class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
 
+    if @user.save
+      render json: { user: @user }, status: :ok
+    else
+      render json: { error: @user.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :password, :email)
+  end
 end
